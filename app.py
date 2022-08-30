f104c082153567ed2dcafff40b2089a1f0b79f61
Israel Miller <israel.miller@stonybrook.edu>
2022-08-30 14:52:54 -0400

    Update app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+#Home Page 
+@app.route('/')
+def home_page():
+    return 'Flask Assignment 1' 
+#Dashboard Page
+@app.route('/dashboard')
+def dashboard_page():
+    return 'This is the Dashboard Page'
+#Login Page
+@app.route('/login')
+def login_page():
+    return 'This is the Login Page'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=80)
